f8b2df686f18dbc6562787bbb272fe39648d6b5c
rblake <rblake@eb005477-0536-0410-a1b3-b73f45257991>
2008-09-19 18:38:58 +0000

    Checking in a version of header guard that actually works without my stupid library.

--- header_guard.pl
+++ header_guard.pl
@@ -1,10 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
 
-use FindBin qw($RealBin);
-use lib "$RealBin/perl_inc";
-use rcb_functions;
-
 my $filename;
 my @infile;
 my $define_flag;
@@ -13,8 +9,9 @@ my @footer;
 
 
 foreach $filename (@ARGV) {
-    @infile = file_to_list($filename);
-    
+    open(FILE, $filename) or die "Can't open file $filename: $!\n";
+    @infile = <FILE>;
+    close(FILE);
 
     if (! ($infile[0] =~ /HEADER GUARD/)) {
 	$define_flag = $filename;
@@ -24,25 +21,23 @@ foreach $filename (@ARGV) {
 	$define_flag = "__" . $define_flag;
 	$define_flag .= "__";
 	
-	@header = (   "//// HEADER GUARD ///////////////////////////"
-		   ,  "// If automatically generated, keep above"
-                   ,  "// comment as first line in file."
-		   ,  "#ifndef $define_flag"
-		   ,  "#define $define_flag"
-		   ,  "//// HEADER GUARD ///////////////////////////"
-		   );
-
-	@footer = (   "//// HEADER GUARD ///////////////////////////"
-		   ,  "#endif"
-                   ,  "//// HEADER GUARD ///////////////////////////"
-		   );
-
-	unshift @infile, @header;
-	push @infile, @footer;
-
-	open(OUTFILE, "> $filename");
-	print OUTFILE make_list_a_file(@infile);
-	print OUTFILE "\n";
-	close OUTFILE;
+	open(OUTFILE, ">$filename") or die "Can't open file $filename for writing: $!\n";
+	print OUTFILE <<_EOF
+//// HEADER GUARD ///////////////////////////
+// If automatically generated, keep above
+// comment as first line in file.
+#ifndef $define_flag
+#define $define_flag
+//// HEADER GUARD ///////////////////////////
+_EOF
+;
+	print OUTFILE join("\n", @infile) . "\n";
+	print OUTFILE <<_EOF
+//// HEADER GUARD ///////////////////////////
+#endif
+//// HEADER GUARD ///////////////////////////
+_EOF
+;
+	close(OUTFILE);
     }
 }
